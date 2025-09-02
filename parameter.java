f872bde3adc0ebc4136f6bf127a4d759d60b6651
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:41:11 +0530

    Add files via upload

---
+++ parameter.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class parameter {
+	static void method() {
+		System.out.println("no parameter and no return value method");
+	}
+	public static void main(String[] args) {
+		method();
+		
+	
+	}
+
+}
