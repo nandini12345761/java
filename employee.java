99c8d8f3e8f13b67c648f98b3cff1a60396ccc81
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:27:22 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,26 @@
+package nandini;
+
+public class employee {
+	String empname;
+	int empnum;
+	double salary;
+	public static void main(String[] args) {
+		employee E1=new employee();
+		E1.empname="keerthi";
+		E1.empnum=3320;
+		E1.salary=50000;
+		System.out.println(E1.empname);
+		System.out.println(E1.empnum);
+		System.out.println(E1.salary);
+		employee E2=new employee();
+		E2.empname="nandini";
+		E2.empnum=3319;
+		E2.salary=70000;
+		System.out.println(E2.empname);
+		System.out.println(E2.empnum);
+		System.out.println(E2.salary);
+		
+		
+	}
+
+}
