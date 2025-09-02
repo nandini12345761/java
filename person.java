c0305a36b98c2e5eda5f4cb2eb8e3eab1d256c08
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:26:50 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,12 @@
+package nandini;
+
+public class person {
+	String name;
+	int age;
+	static String nationality="indian";
+	public static void main(String[] args) {
+		System.out.println(nationality);
+		System.out.println(person.nationality);
+	}
+
+}
