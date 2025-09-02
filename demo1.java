3404c8c30f97981f8742c5b33326e8408c0a7069
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:25:30 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,17 @@
+package nandini;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		int i=99;
+		double d=125.78;
+		System.out.println("d="+d);
+		System.out.println("i="+i);
+		System.out.println("After primitive type conversions");
+		i=(int)d;
+		System.out.println("d="+d);
+		System.out.println("i="+i);
+
+	}
+
+}
