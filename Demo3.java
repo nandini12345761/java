4dee3d0a2d87efdcf211f2d8c8dc663a853f0cac
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:26:11 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,15 @@
+package nandini;
+
+public class Demo3 {
+	static void display() {
+	System.out.println("java");
+	System.out.println("python");
+	System.out.println("SQL");
+	}
+	public static void main(String[] args) {
+	display();
+	display();
+	display();
+	}
+
+}
