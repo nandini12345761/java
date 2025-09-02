a349d5b19aac77c0c2c604d52db2fe2fca60fc19
nandini12345761 <239x1a3320@gmail.com>
2025-09-02 17:19:17 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,17 @@
+package nandini;
+
+public class mobile {
+	String colour="red";
+	double prize=40000;
+	String brand="vivo";
+	String ram="5gb";
+	public static void main(String[] args) {
+		mobile M1=new mobile();
+		System.out.println("mobile colour : "+M1.colour);
+		System.out.println("mobile prize : "+M1.prize);
+		System.out.println("mobile brand : "+M1.brand);
+		System.out.println("mobile ram : "+M1.ram);
+	}
+
+}
+
